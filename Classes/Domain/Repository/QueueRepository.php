7bd9ce7c758ff8a8b23df8009a28d57336a1b413
Patric Eckhart <mail@patriceckhart.com>
2017-04-06 12:09:41 +0200

    no message

--- Classes/Domain/Repository/QueueRepository.php
+++ Classes/Domain/Repository/QueueRepository.php
@@ -43,23 +43,6 @@ class QueueRepository extends Repository
         $classname = '\DirectMail\Newsletter\Domain\Model\Queue';
         $query = $this->persistenceManager->createQueryForType($classname);
         $results = $query->matching($query->equals('send', "0"))->execute()->getFirst();
-        //$results = $query->matching($query->equals('send', "0"))->execute();
-
-        /*while ($result = $results->current()) {
-
-            $resultsenddatetime = $results->getSenddatetime();
-
-            $resultsdttimestamp = strtotime($resultsenddatetime);
-            $resultnow = time();
-            $resultnow = intval($resultnow);
-            $resultsdttimestamp = intval($resultsdttimestamp);
-
-            if ($resultnow<$resultsdttimestamp) {
-
-            }
-
-            $results->next();
-        }*/
 
         $category = $results->getCategory();
         $title = $results->getTitle();
@@ -83,73 +66,73 @@ class QueueRepository extends Repository
             $this->persistenceManager->persistAll();
 
 
-        while ($result2 = $results2->current()) {
-            $email = $result2->getEmail();
-            $user = $result2->getDeleted();
+            while ($result2 = $results2->current()) {
+                $email = $result2->getEmail();
+                $user = $result2->getDeleted();
 
-            $firstname = $result2->getFirstname();
-            $lastname = $result2->getLastname();
-            $gender = $result2->getGender();
+                $firstname = $result2->getFirstname();
+                $lastname = $result2->getLastname();
+                $gender = $result2->getGender();
 
-            $salutation1 = $this->settings['salutation1'];
-            $salutation2 = $this->settings['salutation2'];
-            $salutation3 = $this->settings['salutation3'];
+                $salutation1 = $this->settings['salutation1'];
+                $salutation2 = $this->settings['salutation2'];
+                $salutation3 = $this->settings['salutation3'];
 
-            $presalutation1 = $this->settings['presalutation1'];
-            $presalutation2 = $this->settings['presalutation2'];
-            $presalutation3 = $this->settings['presalutation3'];
+                $presalutation1 = $this->settings['presalutation1'];
+                $presalutation2 = $this->settings['presalutation2'];
+                $presalutation3 = $this->settings['presalutation3'];
 
-            if($gender==1) {
-                $salutationgender = $salutation1;
-                $presalutationgender = $presalutation1;
-            } else if ($gender == 2) {
-                $salutationgender = $salutation2;
-                $presalutationgender = $presalutation2;
-            } else {
-                $salutationgender = $salutation3;
-                $presalutationgender = $presalutation3;
-            }
-
-            if($user=="0") {
-                $file = file_get_contents($pageurl);
-                //$body1 = preg_replace("/.*<body[^>]*>|<\/body>.*/si", "", $file);
-                //$body = $file;
-
-                $salutation = $presalutationgender.' '.$salutationgender.' '.$firstname.' '.$lastname;
-
-                if($firstname == "" || $lastname == "") {
-                    $body = str_replace("{salutation}","",$file);
-                    $body = str_replace("{firstname}","",$body);
-                    $body = str_replace("{lastname}","",$body);
-                    $body = str_replace("{gender}","",$body);
+                if($gender==1) {
+                    $salutationgender = $salutation1;
+                    $presalutationgender = $presalutation1;
+                } else if ($gender == 2) {
+                    $salutationgender = $salutation2;
+                    $presalutationgender = $presalutation2;
                 } else {
-                    $body = str_replace("{salutation}",$salutation,$file);
-                    $body = str_replace("{firstname}",$firstname,$body);
-                    $body = str_replace("{lastname}",$lastname,$body);
-                    $body = str_replace("{gender}",$salutationgender,$body);
+                    $salutationgender = $salutation3;
+                    $presalutationgender = $presalutation3;
                 }
 
-                $subject = $title;
+                if($user=="0") {
+                    $file = file_get_contents($pageurl);
+                    //$body1 = preg_replace("/.*<body[^>]*>|<\/body>.*/si", "", $file);
+                    //$body = $file;
 
-                $mail = new \Neos\SwiftMailer\Message();
-                $mail->setFrom(array($this->settings['senderMail'] => $this->settings['senderName']))
-                    ->setTo(array($email))
-                    ->setReplyTo(array($this->settings['replyToMail'] => $this->settings['replyToName']))
-                    ->setSubject($subject)
-                    ->setBody($body, 'text/html')
-                    ->send();
+                    $salutation = $presalutationgender.' '.$salutationgender.' '.$firstname.' '.$lastname;
 
-                $posted = $results->getPosted();
-                $results->setPosted($posted+1);
-                $this->queueRepository->update($results);
-                $this->persistenceManager->persistAll();
+                    if($firstname == "" || $lastname == "") {
+                        $body = str_replace("{salutation}","",$file);
+                        $body = str_replace("{firstname}","",$body);
+                        $body = str_replace("{lastname}","",$body);
+                        $body = str_replace("{gender}","",$body);
+                    } else {
+                        $body = str_replace("{salutation}",$salutation,$file);
+                        $body = str_replace("{firstname}",$firstname,$body);
+                        $body = str_replace("{lastname}",$lastname,$body);
+                        $body = str_replace("{gender}",$salutationgender,$body);
+                    }
+
+                    $subject = $title;
+
+                    $mail = new \Neos\SwiftMailer\Message();
+                    $mail->setFrom(array($this->settings['senderMail'] => $this->settings['senderName']))
+                        ->setTo(array($email))
+                        ->setReplyTo(array($this->settings['replyToMail'] => $this->settings['replyToName']))
+                        ->setSubject($subject)
+                        ->setBody($body, 'text/html')
+                        ->send();
+
+                    $posted = $results->getPosted();
+                    $results->setPosted($posted+1);
+                    $this->queueRepository->update($results);
+                    $this->persistenceManager->persistAll();
+                }
+
+                $results2->next();
             }
-
-            $results2->next();
-        }
 
         }
 
     }
-
+    
 }
